d2a5b07ab63d8d60d7a02ea98fcfa3468187f758
Darius Mihai <dariusmihai@gmail.com>
2022-08-06 19:22:55 +0300

    fix path on test script

--- scripts/quickstart/start-2-5-1.sh
+++ scripts/quickstart/start-2-5-1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./../init-nikon-d750-sd.sh
-./../timelapse.sh --exposure-time 2 --number-frames 5 --pause-time 1
+../init-nikon-d750-sd.sh
+../timelapse.sh --exposure-time 2 --number-frames 5 --pause-time 1
